9265d7ab9dbb3ff2075e35a3e45dcd27dd95c7db
elskater98 <elskater98@gmail.com>
2020-10-28 11:44:26 +0100

    - Add HomeView - Add SettingsView - Create Bottom Navigation Bar with the created views.

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:reciclame/views/HomeView.dart';
+import 'package:reciclame/views/SettingsView.dart';
+import '../constants.dart';
 
 class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = [
+    HomeView(),
+    SettingsView()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('AppBar'),
+        title: Text('RECICLAME'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const<BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label:'Home'
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kPrimaryColor,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
+
+
---
+++ lib/views/HomeView.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class HomeView extends StatefulWidget {
+  const HomeView();
+  @override
+  _HomeViewState createState() => _HomeViewState();
+
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Home View'),
+    );
+  }
+}
