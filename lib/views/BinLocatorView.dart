fd595099d4948b0ed20101580940892e5188ed2b
Albert <albertperezdatsira@gmail.com>
2020-11-11 19:11:59 +0100

    hide debug band

--- lib/views/BinLocatorView.dart
+++ lib/views/BinLocatorView.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(BinLocatorView());
@@ -12,6 +13,7 @@ class BinLocatorView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Maps',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
