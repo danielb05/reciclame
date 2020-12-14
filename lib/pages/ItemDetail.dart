546a8925f8bb8f487b41125b7915607896a08211
elskater98 <45320338+elskater98@users.noreply.github.com>
2020-12-14 20:10:29 +0100

    Find Product By Material : DONE

--- lib/pages/ItemDetail.dart
+++ lib/pages/ItemDetail.dart
@@ -5,11 +5,13 @@ import 'package:reciclame/localization/language_constants.dart';
 import 'package:reciclame/services/containserService.dart';
 import 'package:reciclame/services/materialService.dart';
 import 'package:reciclame/services/productService.dart';
+import 'package:reciclame/widgets/ItemWidget.dart';
 
 import '../main.dart';
 
 class Item {
-  Item({this.isExpanded = false, this.header = "Description", this.description});
+  Item(
+      {this.isExpanded = false, this.header = "Description", this.description});
 
   bool isExpanded;
   String header;
@@ -37,7 +39,7 @@ class _ItemDetailState extends State<ItemDetail> {
     setState(() {
       listItems.add(new Item(description: widget.arguments["description"]));
     });
-    ProductService.instance.isMaterial(widget.arguments["name"]).then((value){
+    ProductService.instance.isMaterial(widget.arguments["name"]).then((value) {
       setState(() {
         isMaterial = value;
       });
@@ -64,7 +66,8 @@ class _ItemDetailState extends State<ItemDetail> {
   }
 
   Future getProducts() async {
-    var products = await ProductService.instance.getByMaterial(widget.arguments['name']);
+    var products =
+        await ProductService.instance.getByMaterial(widget.arguments['name']);
     return Future.value(products);
   }
 
@@ -79,122 +82,139 @@ class _ItemDetailState extends State<ItemDetail> {
                 ? widget.arguments['name']
                 : widget.arguments['name_ES']),
             centerTitle: true),
-        body: !(isMaterial??false) ? Center(
-            child: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(20),
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                  image: NetworkImage(widget.arguments['pictures'][0]),
-                  fit: BoxFit.fill),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          ExpansionPanelList(
-            expansionCallback: (int index, bool isExpanded) {
-              setState(() {
-                listItems[index].isExpanded = !isExpanded;
-              });
-            },
-            children: listItems.map<ExpansionPanel>((Item item) {
-              return ExpansionPanel(
-                headerBuilder: (BuildContext context, bool isExpanded) {
-                  return ListTile(
-                    title: Text(item.header),
-                  );
-                },
-                body: ListTile(
-                  title: Text(item.description),
-                  subtitle: Text(''),
+        body: !(isMaterial ?? false)
+            ? Center(
+                child: Column(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(20),
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(widget.arguments['pictures'][0]),
+                        fit: BoxFit.fill),
+                  ),
                 ),
-                isExpanded: item.isExpanded,
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 40),
-              FutureBuilder(
-                future: getMaterials(), // function where you call your api
-                builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                  // AsyncSnapshot<Your object type>
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  } else {
-                    if (snapshot.hasError) {
+                SizedBox(height: 10.0),
+                ExpansionPanelList(
+                  expansionCallback: (int index, bool isExpanded) {
+                    setState(() {
+                      listItems[index].isExpanded = !isExpanded;
+                    });
+                  },
+                  children: listItems.map<ExpansionPanel>((Item item) {
+                    return ExpansionPanel(
+                      headerBuilder: (BuildContext context, bool isExpanded) {
+                        return ListTile(
+                          title: Text(item.header),
+                        );
+                      },
+                      body: ListTile(
+                        title: Text(item.description),
+                        subtitle: Text(''),
+                      ),
+                      isExpanded: item.isExpanded,
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 40),
+                FutureBuilder(
+                  future: getMaterials(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
                       return Center(child: CircularProgressIndicator());
                     } else {
-                      return Expanded(
-                        child: new ListView.builder(
-                            itemCount: snapshot.data.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Center(
-                                  child: Chip(
-                                      label:
-                                      Text('${snapshot.data[index]["name"]}')));
-                            }),
-                      );
+                      if (snapshot.hasError) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        return Expanded(
+                          child: new ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Center(
+                                    child: Chip(
+                                        label: Text(
+                                            '${snapshot.data[index]["name"]}')));
+                              }),
+                        );
+                      }
                     }
-                  }
-                },
-              ),
-          Divider(color: Colors.black,),
-          FutureBuilder(
-            future: getContainers(), // function where you call your api
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-              // AsyncSnapshot<Your object type>
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                if (snapshot.hasError) {
-                  return Center(child: CircularProgressIndicator());
-                } else {
-                  return Expanded(
-                    child: new ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Center(
-                              child: Chip(
-                                  label:
-                                      Text('${snapshot.data[index]["name"]}'),
-                                  backgroundColor:
-                                      HexColor(snapshot.data[index]["color"])));
-                        }),
-                  );
-                }
-              }
-            },
-          ),
-          RaisedButton.icon(
-            color: kPrimaryColor,
-            onPressed: () {
-              Navigator.pop(context);
-              Navigator.pushReplacementNamed(context, '/home');
-            },
-            label: Text(getTranslated(context, 'title').toUpperCase()),
-            icon: Icon(Icons.restore_from_trash),
-          ),
-          SizedBox(height: 40)
-        ])):Center(
-          child: FutureBuilder(
-          future: getProducts(), // function where you call your api
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            // AsyncSnapshot<Your object type>
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else {
-              if (snapshot.hasError) {
-                return Center(child: CircularProgressIndicator());
-              } else {
-                return Text(snapshot.data.toString())
-                ;
-              }
-            }
-          },
-        ),
-
-
-    ));
+                  },
+                ),
+                Divider(
+                  color: Colors.black,
+                ),
+                FutureBuilder(
+                  future: getContainers(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else {
+                      if (snapshot.hasError) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        return Expanded(
+                          child: new ListView.builder(
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Center(
+                                    child: Chip(
+                                        label: Text(
+                                            '${snapshot.data[index]["name"]}'),
+                                        backgroundColor: HexColor(
+                                            snapshot.data[index]["color"])));
+                              }),
+                        );
+                      }
+                    }
+                  },
+                ),
+                RaisedButton.icon(
+                  color: kPrimaryColor,
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pushReplacementNamed(context, '/home');
+                  },
+                  label: Text(getTranslated(context, 'title').toUpperCase()),
+                  icon: Icon(Icons.restore_from_trash),
+                ),
+                SizedBox(height: 40)
+              ]))
+            : Center(
+                child: FutureBuilder(
+                  future: getProducts(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else {
+                      if (snapshot.hasError) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        return Expanded(
+                            child: ListView.separated(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                              onTap: () {
+                                print(snapshot.data[index]);
+                                Navigator.pushReplacementNamed(context, '/item',
+                                    arguments: snapshot.data[index]);
+                              },
+                              child: ItemWidget(entries: snapshot.data[index]),
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
+                        ));
+                      }
+                    }
+                  },
+                ),
+              ));
   }
 }
