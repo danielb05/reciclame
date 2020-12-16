52e2367b50e94cc7cf1dec5161baa98f10dcd3e3
elskater98 <45320338+elskater98@users.noreply.github.com>
2020-12-16 20:00:43 +0100

    Generate pie chart

--- lib/services/historyService.dart
+++ lib/services/historyService.dart
@@ -12,19 +12,23 @@ class HistoryService{
   getHistory() async {
 
     if(FirebaseAuth.instance.currentUser != null){
-      var historic = {};
+      List<Map<String,dynamic>> historic = new List();
 
       var materialsRefs = await MaterialService.instance.getAllMaterialsReferences();
-      
+      var aux = await FirebaseFirestore.instance.collection('history').where("uid", isEqualTo: FirebaseAuth.instance.currentUser.uid).get();
+      var total = aux.docs.length;
       for(var material in materialsRefs){
         var snapshot = await FirebaseFirestore.instance.collection('history')
             .where("uid", isEqualTo: FirebaseAuth.instance.currentUser.uid)
             .where("material", isEqualTo: material)
             .get();
+
         var materialName = await MaterialService.instance.getByReference(material);
-        historic[materialName["name"]] = snapshot.docs.length;
+        historic.add({"name":materialName["name"], "quantity":snapshot.docs.length,"total":total});
       }
       return historic;
     }
   }
+
+
 }
